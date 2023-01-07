08d4597bd5e5a8ede054bce3aad9ffad4af673f5
coguars <122156250+coguars@users.noreply.github.com>
2023-01-07 12:53:42 +0100

    File iniziali per il progetto MakeCode

---
+++ main.ts
@@ -0,0 +1,70 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (receivedNumber == 1) {
+        Kitronik_Move_Motor.stop()
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(750)
+        basic.clearScreen()
+    } else if (receivedNumber == 2) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 3) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 50)
+        basic.pause(100)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (receivedNumber == 4) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Left, 50)
+        basic.pause(100)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (receivedNumber == 5) {
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 20)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # # .
+            . . # # #
+            `)
+    } else if (receivedNumber == 6) {
+        speed = 50
+    } else if (receivedNumber == 7) {
+        speed = 100
+    }
+})
+let speed = 0
+speed = 50
+radio.setGroup(100)
